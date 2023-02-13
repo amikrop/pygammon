9fc647db90f4766dba9f2b6e80dce065c5016ceb
Aristotelis Mikropoulos <amikrop@gmail.com>
2023-02-13 19:37:49 +0200

    v0.0.2

--- pygammon/__init__.py
+++ pygammon/__init__.py
@@ -2,6 +2,6 @@ from pygammon.core import run
 from pygammon.structures import GameState, InputType, InvalidMoveCode, OutputType, Side
 
 __all__ = ["GameState", "InputType", "InvalidMoveCode", "OutputType", "Side", "run"]
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __author__ = "Aristotelis Mikropoulos <amikrop@gmail.com>"
 __license__ = "MIT"
